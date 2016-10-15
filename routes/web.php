d40d8eedbd993195b365e34f2ef3820d444e047e
Mitchfizz05 <me@mitchfizz05.net>
2016-10-15 20:13:48 +1030

    Added the debug route /debug/stopdata/{stopcode} (only availabe is APP_DEBUG is true) that gets the SiriVisits for that stop and dumps them.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+if (config('app.debug')) {
+
+    // Test route that dumps realtime data for a particular stop code.
+    Route::get('/debug/stopdata/{id}', function ($id) {
+        $client = new \App\Siri\SiriClient();
+        $resp = $client->getRealtimeData($id);
+        var_dump($resp);
+    });
+
+}
